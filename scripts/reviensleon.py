93f49f9ca0bee6edf1b7974b71dc1ce7815d43c4
vincentsarago <vincent.sarago@gmail.com>
2016-07-01 08:41:47 -0400

    Corrections

--- scripts/reviensleon.py
+++ scripts/reviensleon.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
+
 import os
+import time
+from osgeo import ogr, osr
 from urllib2 import urlopen
 import bs4 as BeautifulSoup
+from boto3.session import Session
 from geopy.geocoders import Nominatim
-from osgeo import ogr, osr
+
+#AWS S3 Account Informations
+session = Session(aws_access_key_id='',
+                  aws_secret_access_key='',
+                  region_name='')
 
 driver = ogr.GetDriverByName('GeoJSON')
-outjson = 'reviensleon.geojson'
+outjson = '/tmp/reviensleon.geojson'
 if os.path.isfile(outjson):
     os.remove(outjson)
 
@@ -36,13 +44,18 @@ while True:
     for o in offres:
         ep = (o.find("p", attrs={"class":"mt10"}).text).split(' / ')
         place = ep[1] if len(ep) == 2 else ''
+        lat = 0
+        lon = 0
         if place != '':
-            location = geolocator.geocode(place)
-            lat = location.latitude
-            lon = location.longitude
-        else:
-            lat = 0
-            lon = 0
+            print(place)
+            try:
+                location = geolocator.geocode(place)
+            except:
+                print("Geopy Error for {}".format(place))
+            if location:
+                lat = location.latitude
+                lon = location.longitude
+            time.sleep(1) #sleep 1 sec to not overcall Nominatim
 
         feature = ogr.Feature(defn)
         point = ogr.Geometry(ogr.wkbPoint)
@@ -54,10 +67,10 @@ while True:
         employeur = a.get("data-employeur").encode('utf-8')
         link = a.get("href").encode('utf-8')
 
-        feature.SetField('titre', titre)
-        feature.SetField('employeur', employeur)
-        feature.SetField('link', link)
-        feature.SetField('place', place)
+        feature.SetField("titre", titre)
+        feature.SetField("employeur", employeur)
+        feature.SetField("link", link)
+        feature.SetField("place", place.encode('utf-8'))
         layer.CreateFeature(feature)
 
     del soup
@@ -65,3 +78,10 @@ while True:
 
 layer = None
 json_ds.Destroy()
+
+#Upload image to S3
+if os.path.exists(outjson):
+    s3 = session.resource('s3')
+    fname = 'data/{}'.format(os.path.basename(outjson))
+    s3.Bucket('remotepixel').upload_file(outjson, fname, ExtraArgs={'ACL': 'public-read'})
+    print("file uploaded to AWS S3")
